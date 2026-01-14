e54a25d84bf962db88d7f9806c8698687352b88c
superlucky24 <ghparc24@gmail.com>
2026-01-14 18:45:13 +0900

    민원 게시판 페이징 기능 상세보기까지 추가

--- src/main/java/kr/go/ydpb/controller/ComplaintController.java
+++ src/main/java/kr/go/ydpb/controller/ComplaintController.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("admin")
@@ -28,5 +30,14 @@ public class ComplaintController {
         model.addAttribute("pageMaker",new PageDTO(cri,total));
 
         return "admin/admin_complaint_list";
+    }
+
+
+    @GetMapping("complaint/view")
+    public String complaintView(@RequestParam("comId") int comId,
+                                @ModelAttribute("cri") Criteria cri, Model model){
+        model.addAttribute("complaint",complaintService.getOneComplaint(comId));
+
+        return "admin/admin_complaint_view";
     }
 }

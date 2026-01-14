811fb2234b520737510789c6d787fee61a4a4e99
superlucky24 <ghparc24@gmail.com>
2026-01-14 16:43:14 +0900

    민원 처리 관련 작업 분리 추가

--- src/main/java/kr/go/ydpb/controller/ComplaintController.java
+++ src/main/java/kr/go/ydpb/controller/ComplaintController.java
@@ -2,8 +2,8 @@ package kr.go.ydpb.controller;
 
 import kr.go.ydpb.domain.Criteria;
 import kr.go.ydpb.domain.PageDTO;
-import kr.go.ydpb.mapper.AdminMapper;
 import kr.go.ydpb.service.AdminService;
+import kr.go.ydpb.service.ComplaintService;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +15,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("admin")
 @AllArgsConstructor
-public class AdminController {
+public class ComplaintController {
     //주입
     @Setter(onMethod_ = @Autowired)
-    private AdminService adminService;
+    private ComplaintService complaintService;
 
+    //매핑
+    @GetMapping("complaint/list")
+    public String complaintList(Model model, Criteria cri){
+        model.addAttribute("complaintList", complaintService.getComplaintWithPaging(cri));
+        int total = complaintService.getAllCount(cri);
+        model.addAttribute("pageMaker",new PageDTO(cri,total));
 
+        return "admin/admin_complaint_list";
+    }
 }

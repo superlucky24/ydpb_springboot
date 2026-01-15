ad681a8ed38d574cf7f129464eb5639b2f5fec17
yoonsungmin <bill5680@naver.com>
2026-01-15 15:34:21 +0900

    멤버 컨트롤러 기능 추가

--- src/main/java/kr/go/ydpb/controller/MemberController.java
+++ src/main/java/kr/go/ydpb/controller/MemberController.java
@@ -1,4 +1,19 @@
 package kr.go.ydpb.controller;
 
+
+import kr.go.ydpb.domain.Criteria;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
 public class MemberController {
+    @GetMapping("/login")
+    public String login(Criteria cri, Model model) {
+
+
+    }
 }

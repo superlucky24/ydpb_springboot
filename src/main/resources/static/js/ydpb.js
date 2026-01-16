513df1e08dcaef825b532a465fb8d9c7aa625beb
YeonSu <hades466@gmail.com>
2026-01-16 11:23:17 +0900

    사이드메뉴 민원안내 추가 html / js / css

--- src/main/resources/static/js/ydpb.js
+++ src/main/resources/static/js/ydpb.js
@@ -73,13 +73,16 @@ function initUi() {
         $(this).parent('.side_list_menu').siblings().removeClass('open');
         $(this).parent().siblings().find('.sub_list').removeClass('show');
         $(this).parent().siblings().find('.sub_group').removeClass('show');
+        $(this).parent().siblings().find('.sub_complaint').removeClass('show');
         $(this).next('.sub_list').toggleClass('show');
         $(this).next().find('.sub_group').toggleClass('show');
+        $(this).next().find('.sub_complaint').toggleClass('show');
         $(this).parent().toggleClass('open');
     });
 
     $('.sub_group_title').on('click', function(){
         $('.sub_group_title').not(this).removeClass('open');
+        $('.sub_complaint_title').not(this).removeClass('open');
         $('.sub_group_list').not($(this).next('.sub_group_list')).removeClass('show');
         $(this).toggleClass('open');
         $(this).next('.sub_group_list').toggleClass('show');

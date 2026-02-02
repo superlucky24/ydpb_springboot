eee4565a61387d453a124ef1f7840da09169fe5a
superlucky24 <ghparc24@gmail.com>
2026-02-02 14:26:18 +0900

    귀환 일반 로그인 spring security 적용

--- src/main/java/kr/go/ydpb/service/UserDetailsServiceImpl.java
+++ src/main/java/kr/go/ydpb/service/UserDetailsServiceImpl.java
@@ -11,32 +11,32 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-//@Service
-//@RequiredArgsConstructor
-//public class UserDetailsServiceImpl implements UserDetailsService {
-//
-//    private final MemberMapper memberMapper;
-//
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        System.out.println("### loadUserByUsername CALLED");
-//        MemberVO member = memberMapper.Login(username);
-//        System.out.println("DB PW = " + member.getMemPassword());
-//        if (member == null) {
-//            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username);
-//        }
-//
-//        String role = member.getMemRole() == 1
-//                ? "ROLE_ADMIN"
-//                : "ROLE_USER";
-//
-//        System.out.println("role => " + role);
-//
-//        return User.builder()
-//                .username(member.getMemId())
-//                .password(member.getMemPassword())
-//                .roles(member.getMemRole() == 1 ? "ADMIN" : "USER")
-//                .build();
-//    }
-//}
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberMapper memberMapper;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String memId) throws UsernameNotFoundException {
+        System.out.println("loadUserByUsername CALLED");
+        MemberVO member = memberMapper.Login(memId);
+        System.out.println("DB PW = " + member.getMemPassword());
+        if (member == null) {
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + memId);
+        }
+
+        String role = member.getMemRole() == 1
+                ? "ROLE_ADMIN"
+                : "ROLE_USER";
+
+        System.out.println("role => " + role);
+
+        return User.builder()
+                .username(member.getMemId())
+                .password(member.getMemPassword())
+                .roles(member.getMemRole() == 1 ? "ADMIN" : "USER")
+                .build();
+    }
+}
